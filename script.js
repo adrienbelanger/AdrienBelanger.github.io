0ad3fd0fc71448dc8c81db469a31928948fac518
Adrien.gmail <adribelanger@gmail.com>
2024-03-07 15:37:59 -0500

    Added scroll track sidebar

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ function closeSidebar() {
     sidebar.classList.remove('open');
     overlay.style.display = "none"; // Hide overlay
 }
+
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    var container = document.getElementById("header-sidebar-container");
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        container.style.background = "rgba(255, 255, 255, 0.9)";
+        container.style.padding = "10px 0"; // Adjust padding when scrolling
+    } else {
+        container.style.background = "none";
+        container.style.padding = "20px 0"; // Reset padding when not scrolling
+    }
+}
+
